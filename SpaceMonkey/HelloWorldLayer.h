fb2451ba321be9964ceab30aedf3a77f4f57821b
Yasin Tarim <yasintar@gmail.com>
2011-07-16 13:28:08 +0300

    box2d eklendi

--- SpaceMonkey/HelloWorldLayer.h
+++ SpaceMonkey/HelloWorldLayer.h
@@ -9,13 +9,17 @@
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
+#import "Box2D.h"
 #import "PauseMenuButtonLayer.h"
 
 // HelloWorldLayer
 @interface HelloWorldLayer : CCLayer
 {
-	CCSprite *m_menuButton;
-	CCSprite *m_backgroundImg;
+	CCSprite* m_menuButton;
+	CCSprite* m_backgroundImg;
+	CCSprite* m_ball;
+	b2World* m_world;
+	b2Body* m_body;
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
--- SpaceMonkey/HelloWorldLayer.mm
+++ SpaceMonkey/HelloWorldLayer.mm
@@ -6,7 +6,7 @@
 //  Copyright YKB 2011. All rights reserved.
 //
 
-
+#define PTM_RATIO 32.0
 // Import the interfaces
 #import "HelloWorldLayer.h"
 
@@ -41,13 +41,89 @@
 		[self addChild:menuButton z:2];
 		m_backgroundImg.position = ccp(winSize.width/2, winSize.height/2);
 		[self addChild:m_backgroundImg z:1];
+		
+		
+		m_ball = [CCSprite spriteWithFile:@"Ball.jpg" rect:CGRectMake(0, 0, 52, 52)];
+		m_ball.position = ccp(100, 100);
+		[self addChild:m_ball z:2];
+		
+		//create a world
+	
+		b2Vec2 gravity = b2Vec2(0.0f,-30.0f);
+		bool doSleep = true;
+		m_world = new b2World(gravity, doSleep);
+		
+		//create edges around the screen
+		
+		b2BodyDef groundBodyDef;
+		groundBodyDef.position.Set(0, 0);
+		
+		b2Body* groundBody = m_world->CreateBody(&groundBodyDef);
+		
+		b2PolygonShape groundBox;
+		b2FixtureDef boxShapeDef;
+		boxShapeDef.shape = &groundBox;
+		
+		float heightRatio = winSize.height / PTM_RATIO;
+		float widthRatio = winSize.width / PTM_RATIO;
+		
+		
+		groundBox.SetAsEdge(b2Vec2(0, 0), b2Vec2(widthRatio, 0));
+		groundBody->CreateFixture(&boxShapeDef);
+		
+		groundBox.SetAsEdge(b2Vec2(0,0), b2Vec2(0, heightRatio));
+		groundBody->CreateFixture(&boxShapeDef);
+		
+		groundBox.SetAsEdge(b2Vec2(0, heightRatio), b2Vec2(widthRatio, heightRatio));
+		groundBody->CreateFixture(&boxShapeDef);
+		
+		groundBox.SetAsEdge(b2Vec2(widthRatio, heightRatio), b2Vec2(widthRatio, 0));
+		groundBody->CreateFixture(&boxShapeDef);
+		
+		
+		b2BodyDef ballBodyDef;
+		ballBodyDef.type = b2_dynamicBody;
+		ballBodyDef.position.Set(100/PTM_RATIO, 100/PTM_RATIO);
+		ballBodyDef.userData = m_ball;
+		m_body = m_world->CreateBody(&ballBodyDef);
+		
+		b2CircleShape circle;
+		circle.m_radius = 26.0 / PTM_RATIO;
+		
+		b2FixtureDef ballShapeDef;
+		ballShapeDef.shape = &circle;
+		ballShapeDef.density = 1.0f;
+		ballShapeDef.friction = 0.2f;
+		ballShapeDef.restitution = 0.8f;
+		
+		m_body->CreateFixture(&ballShapeDef);
+
+
+		[self schedule:@selector(tick:)];
+		
 	}
 	return self;
+}
+- (void)tick:(ccTime) dt {
+	
+    m_world->Step(dt, 10, 10);
+    for(b2Body *b = m_world->GetBodyList(); b; b=b->GetNext()) {    
+        if (b->GetUserData() != NULL) {
+            CCSprite *ballData = (CCSprite *)b->GetUserData();
+            ballData.position = ccp(b->GetPosition().x * PTM_RATIO,
+                                    b->GetPosition().y * PTM_RATIO);
+            ballData.rotation = -1 * CC_RADIANS_TO_DEGREES(b->GetAngle());
+        }        
+    }
+	
 }
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
 {
+	delete m_world;
+    m_body = NULL;
+    m_world = NULL;
 	// don't forget to call "super dealloc"
 	[super dealloc];
 }
